c53304429ab914edf976974ac1bbe02a729c1050
Vova <buriyvo@gmail.com>
2018-06-05 01:01:15 +0300

    initial commit

---
+++ ChartSurface/ChartSurface/UserControl1.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChartSurface
+{    
+    public partial class ChartSurface : UserControl
+    {
+        public ChartSurface()
+        {
+            InitializeComponent();      
+        }
+
+        public static DependencyProperty LineDataProperty;
+        public static DependencyProperty LineThicknessProperty;
+        public static DependencyProperty LineBrushProperty;
+        public static DependencyProperty FontFamilyProperty;
+        public static DependencyProperty FontSizeProperty;
+        public static DependencyProperty XAxisTitleProperty;
+        public static DependencyProperty YAxisTitleProperty;
+
+        static ChartSurface()
+        {           
+            LineDataProperty = DependencyProperty.Register("LineData", typeof(IEnumerable<double>), typeof(ChartSurface));
+            LineThicknessProperty = DependencyProperty.Register("LineThickness", typeof(Double), typeof(ChartSurface));
+            LineBrushProperty = DependencyProperty.Register("LineBrush", typeof(Brush), typeof(ChartSurface));
+            FontFamilyProperty = DependencyProperty.Register("FontFamily", typeof(String), typeof(ChartSurface));
+            FontSizeProperty = DependencyProperty.Register("FontSize", typeof(double), typeof(ChartSurface));
+            XAxisTitleProperty = DependencyProperty.Register("XAxisTitle", typeof(string), typeof(ChartSurface));
+            YAxisTitleProperty = DependencyProperty.Register("YAxisTitle", typeof(string), typeof(ChartSurface));
+
+        }
+    
+        public IEnumerable<double> LineData
+        {
+            get { return (IEnumerable<double>)GetValue(LineDataProperty); }
+            set { SetValue(LineDataProperty, value); }
+        }
+        public Double LineThickness
+        {
+            get { return (Double)GetValue(LineThicknessProperty); }
+            set { SetValue(LineThicknessProperty, value); }
+        }
+        public Brush LineBrush
+        {
+            get { return (Brush)GetValue(LineBrushProperty); }
+            set { SetValue(LineBrushProperty, value); }
+        }
+        public String FontFamily
+        {
+            get { return (String)GetValue(FontFamilyProperty); }
+            set { SetValue(FontFamilyProperty, value); }
+        }
+        public double FontSize
+        {
+            get { return (double)GetValue(FontSizeProperty); }
+            set { SetValue(FontSizeProperty, value); }
+        }
+        public string XAxisTitle
+        {
+            get { return (string)GetValue(XAxisTitleProperty); }
+            set { SetValue(XAxisTitleProperty, value); }
+        }
+        public string YAxisTitle
+        {
+            get { return (string)GetValue(YAxisTitleProperty); }
+            set { SetValue(YAxisTitleProperty, value); }
+        }
+    }
+
+    public class DoubleIEnumerableToPointCollection : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            PointCollection p = new PointCollection();
+            bool f = false;
+            double x = 0;
+            foreach (double cdnt in (IEnumerable<double>)value)
+            {
+                if (f == false)
+                {
+                    x = cdnt*50;
+                    f = true;
+                    continue;
+                }
+                else
+                {
+                    p.Add(new Point(x, cdnt*50));
+                    f = false;
+                }
+            }
+            return p;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ ChartSurface/DemoApp/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ChartSurface.ChartSurface cs;
+        double[] data;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            this.FontSizeCB.SelectionChanged += new SelectionChangedEventHandler(OnFontSizeCBChanged);
+            this.FontFamilyCB.SelectionChanged += new SelectionChangedEventHandler(OnFontFamilyCBChanged);
+            this.LineBrashCB.SelectionChanged += new SelectionChangedEventHandler(OnLineBrashCBChanged);
+            this.LineThicknessCB.SelectionChanged += new SelectionChangedEventHandler(OnLineThicknessCBChanged);
+            this.XAxisTitleTB.TextChanged += new TextChangedEventHandler(OnXAxisTitleTBChanged);
+            this.YAxisTitleTB.TextChanged += new TextChangedEventHandler(OnYAxisTitleTBChanged);
+            this.SinewaveRB.Checked += new RoutedEventHandler(OnTWaveRBChecked);
+            this.SquarewaveRB.Checked += new RoutedEventHandler(OnTWaveRBChecked);
+
+            cs = new ChartSurface.ChartSurface
+            {
+                LineData = new Double[] { 0, 6, 1, 2, 2, 4.6, 3, 3.5, 4, 1.9, 5, 3, 6, 5, 7, 9, 8, 4, 9, 2, 10, 3.5 },
+                LineThickness = 2,
+                LineBrush = Brushes.DarkRed,
+                FontFamily = "Comic Sans MS",
+                FontSize = 15,
+                XAxisTitle = this.XAxisTitleTB.Text,
+                YAxisTitle = this.YAxisTitleTB.Text
+            };
+            this.DataContext = cs;
+            data = cs.LineData.ToArray();
+        }
+
+        private void OnTWaveRBChecked(object sender, RoutedEventArgs e)
+        {
+            if (this.SinewaveRB.IsChecked == true)
+                cs.LineData = data;
+            else
+                cs.LineData = CreateStepLineSeries(data);
+        }
+
+        private void OnYAxisTitleTBChanged(object sender, TextChangedEventArgs e)
+        {
+            cs.YAxisTitle = (sender as TextBox).Text;
+        }
+
+        private void OnXAxisTitleTBChanged(object sender, TextChangedEventArgs e)
+        {
+            cs.XAxisTitle = (sender as TextBox).Text;
+        }
+
+        private void OnLineThicknessCBChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cs.LineThickness = Convert.ToDouble(((sender as ComboBox).SelectedItem as ComboBoxItem).Content as string);
+        }
+
+        private void OnLineBrashCBChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cs.LineBrush = ((sender as ComboBox).SelectedItem as TextBlock).Background as Brush;
+        }
+
+        private void OnFontFamilyCBChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cs.FontFamily = ((sender as ComboBox).SelectedItem as ComboBoxItem).Content as string;
+        }
+
+        private void OnFontSizeCBChanged(object sender, SelectionChangedEventArgs e)
+        {           
+            cs.FontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as ComboBoxItem).Content as string);
+        }
+
+        double[] CreateStepLineSeries(double[] source)
+        {
+            List<double> returnValue = new List<double>();
+            for (int i = 1; i < source.Length; i+=2)
+            {
+                double currentValueX = source[i - 1];
+                double currentValueY = source[i];
+                returnValue.Add(currentValueX);
+                returnValue.Add(currentValueY);
+                if (i < source.Length - 1)
+                {
+                    double nextValueX = source[i + 1];
+                    returnValue.Add(nextValueX);
+                    returnValue.Add(currentValueY);
+                }
+            }
+            return returnValue.ToArray();
+        }     
+    }
+}
